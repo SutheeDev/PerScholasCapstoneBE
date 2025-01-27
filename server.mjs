b46fe37134f9c9806710bceb6296b49fcea57468
SU-T <guitaristpeet@gmail.com>
2025-01-27 09:55:39 -0500

    basic setup in server.mjs

--- server.mjs
+++ server.mjs
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+  res.send("Invalid Route : 404");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port : ${port}`);
+});
