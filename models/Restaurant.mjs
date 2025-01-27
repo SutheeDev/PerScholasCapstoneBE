1cfe74cc3ea12973bf092fc4874257712e0a53c1
SU-T <guitaristpeet@gmail.com>
2025-01-27 12:13:51 -0500

    created seed files

--- models/Restaurant.mjs
+++ models/Restaurant.mjs
@@ -1,38 +1,48 @@
 import mongoose from "mongoose";
 
-const restaurantSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
+const restaurantSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    cusine: {
+      type: String,
+    },
+    visitDate: {
+      type: Date,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    review: {
+      type: String,
+    },
+    priceRange: {
+      type: String,
+      enum: [$, $$, $$$, $$$$],
+    },
+    image: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  cusine: {
-    type: String,
-  },
-  visitDate: {
-    type: Date,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-    required: true,
-  },
-  review: {
-    type: String,
-  },
-  priceRange: {
-    type: String,
-    enum: [$, $$, $$$, $$$$],
-  },
-  image: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamp: true }
+);
 
 export default mongoose.model("Restaurant", restaurantSchema);
