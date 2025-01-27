4736b8208f5fd7e2b44c97f417f84b1eccb37bc4
SU-T <guitaristpeet@gmail.com>
2025-01-27 15:26:48 -0500

    created get restaurants route

--- routes/restaurantRoutes.mjs
+++ routes/restaurantRoutes.mjs
@@ -5,6 +5,7 @@ import User from "../models/User.mjs";
 const router = express.Router();
 
 // Create
+// Create restaurant
 router.post("/:id", async (req, res) => {
   try {
     const userId = req.params.id;
@@ -25,6 +26,16 @@ router.post("/:id", async (req, res) => {
 });
 
 // Read
+// Get user restaurants
+router.get("/:userId", async (req, res) => {
+  const restaurants = await Restaurant.find({ userId: req.params.userId });
+
+  if (!restaurants || restaurants.length === 0) {
+    return res.status(400).json("You don't have any restaurant yet");
+  }
+
+  res.status(200).json(restaurants);
+});
 
 // Update
 
