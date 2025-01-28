c2a4c01fcfa1e834d9cfb4b48d88491b912dd966
SU-T <guitaristpeet@gmail.com>
2025-01-28 15:29:45 -0500

    created getUser route and fixed udpateUser

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -3,15 +3,37 @@ import User from "../models/User.mjs";
 
 const router = express.Router();
 
-router.put("/:id", async (req, res) => {
+// getUser route
+router.get("/:id", async (req, res) => {
   try {
-    const updatedUser = await User.findOneAndUpdate(req.params.id, req.body, {
+    const user = await User.findById(req.params.id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
+});
+
+// updateUser route
+router.patch("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json("Please fill in all required fields");
+    }
+
+    const updatedUser = await User.findOneAndUpdate({ _id: userId }, req.body, {
       new: true,
     });
 
+    if (!updatedUser) {
+      return res.status(400).json("No updatedUserrrr!");
+    }
+
     res.status(200).json(updatedUser);
   } catch (err) {
-    console.error(err);
     res.status(500).json("Server Error");
   }
 });
