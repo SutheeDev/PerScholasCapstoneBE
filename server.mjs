04e0dc5dfc7831786b443bdd239a9ad1b5ac4065
SU-T <guitaristpeet@gmail.com>
2025-02-04 12:16:51 -0500

    removed cors origin

--- server.mjs
+++ server.mjs
@@ -17,7 +17,7 @@ import userRouter from "./routes/userRoutes.mjs";
 import restaurantRouter from "./routes/restaurantRoutes.mjs";
 
 app.use(express.json());
-app.use(cors({ origin: "https://perscholascapstonefe.onrender.com/" }));
+app.use(cors());
 
 app.use("/api/user", userRouter);
 app.use("/api/restaurants", restaurantRouter);
